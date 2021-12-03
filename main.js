6cbb3c2615563b753af43506b88ba97c59c215f9
Jonathan Zehner <jonathan.zehner@ufl.edu>
2021-12-03 16:22:38 -0500

    Begin main.js api call

---
+++ main.js
@@ -0,0 +1,19 @@
+// assign var to #weather and button elements
+var wx = document.getElementById('weather')
+var btn = document.querySelector('button')
+
+// render Name of searched city w/ country code, current wx description, icon image for current wx conditions, current and feels like temperatures
+
+btn.onclick = function() {
+    console.log('clicked');
+    // fetch('https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid=cc1adc079d41a52073b50dca9aa7fab3')
+    // .then(function(res) {
+    //     return res.json()
+    // })
+    // .then(function(res) {
+    //     console.log(res.results)
+    // })
+    // .then(function() {
+
+    // })
+}
